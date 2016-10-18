cdca2453018101140de882e0c6dbb183835c891e
wagner ramos <h@des666>
2016-10-18 12:43:59 -0200

    novo projeto add

--- BR/BR_old/index_.php
+++ BR/BR_old/index_.php
@@ -5,14 +5,13 @@
 <!--<meta name="viewport" content="width=1003, initial-scale=1, maximum-scale=1">-->
 <meta name="viewport" content="width=device-width" />
 <title>Wagner Ramos</title>
-<meta name="google-site-verification" content="PmNgDjniblQEvfZuzaCqJvkYj339kXBXFYbjkWiaOBM" />
 <meta name="description" content="Portfólio Wagner Ramos" />
 <meta name="keywords" content="Front-end, Front end, web design, Web Designer, css, Jquery, desenvolvimento, site" />
 <meta name="author" content="Wagner Ramos" />
 <link rel="shortcut icon" href="favicon.ico">
-<link rel="stylesheet" type="text/css" href="css/main.min.css" />
+<link rel="stylesheet" type="text/css" href="css/main.css" />
 <meta property="og:url" content="http://www.wagneramos.com.br/BR"/>
-<meta property="og:image" content="http://www.wagneramos.com.br/BR/images/logo_wagner_ramos.png" />
+<meta property="og:image" content="http://www.wagneramos.com.br/BR/images_m/logo_wagner_ramos.png" />
 <meta property="og:image:type" content="image/png" />
 <meta property="og:image:width" content="271" />
 <meta property="og:image:height" content="72" />
@@ -21,12 +20,12 @@
 <body data-stellar-background-ratio="0.2">
 <div class="cbp-af-header" id="header">
   <div class="cbp-af-inner">
-    <h1 title="Portfólio Wagner Ramos"><a href="#"><img src="images/logo_wagner_ramos.png" alt="Portfólio Wagner Ramos"></a></h1>
+    <h1 title="Portfólio Wagner Ramos"><a href="#"><img src="images_m/logo_wagner_ramos.png" alt="Portfólio Wagner Ramos"></a></h1>
     <div class="languages">
       <ul>
-        <li><a href="index.php" title="Idioma Português (Brasil)"><img src="images/Flag_Brazil.png" alt="Idioma Português (Brasil)"></a></li>
-        <li><a href="../EN/index.php" title="Idioma Inglês"><img src="images/flag_usa.png" alt="Idioma Inglês"></a></li>
-        <li><a href="../DE/index.php" title="Idioma Alemão"><img src="images/bandeira-alemanha-gr.png" alt="Idioma Alemão" class="flag_german"></a></li>
+        <li><a href="index.php" title="Idioma Português (Brasil)"><img src="images_m/Flag_Brazil.png" alt="Idioma Português (Brasil)"></a></li>
+        <li><a href="../EN/index.php" title="Idioma Inglês"><img src="images_m/flag_usa.png" alt="Idioma Inglês"></a></li>
+        <li><a href="../DE/index.php" title="Idioma Alemão"><img src="images_m/bandeira-alemanha-gr.png" alt="Idioma Alemão" class="flag_german"></a></li>
       </ul>
     </div>
     <nav id="nav" role="navigation">
@@ -45,14 +44,14 @@
 
 <div class="flexslider" id="slider" role="banner">
   <ul class="slides">
-    <li><img src="images/banners/lobrobo.jpg" alt="Lobrobo" /></li>
-    <li><img src="images/banners/asus.jpg" alt="Asus" /></li>
-    <li><img src="images/banners/TIM.jpg" alt="Tim" /></li>
-    <li><img src="images/banners/bmw.jpg" alt="BMW"/></li>
-    <li><img src="images/banners/jobs_umbro.jpg" alt="Umbro"/></li>
-    <li><img src="images/banners/topbrands.jpg" alt="Top Brands"/></li>
-    <li><img src="images/banners/ebs.jpg" alt="EBS Solutions"/></li>
-    <li><img src="images/banners/magariblu.jpg" alt="MagariBlu"/></li>
+    <li><img src="images_m/banners/asus.jpg" alt="Asus" /></li>
+    <li><img src="images_m/banners/TIM.jpg" alt="Tim" /></li>
+    <li><img src="images_m/banners/bmw.jpg" alt="BMW"/></li>
+    <li><img src="images_m/banners/jobs_umbro.jpg" alt="Umbro"/></li>
+    <li><img src="images_m/banners/rolinardi.jpg" alt="Rolinardi"/></li>
+    <li><img src="images_m/banners/topbrands.jpg" alt="Top Brands"/></li>
+    <li><img src="images_m/banners/ebs.jpg" alt="EBS Solutions"/></li>
+    <li><img src="images_m/banners/magariblu.jpg" alt="MagariBlu"/></li>
   </ul>
 </div>
 <!--end destaques-->
@@ -70,19 +69,19 @@
 
 <div class="dialog_full photo" data-stellar-background-ratio="0.5">
   <div class="dialog_inner">
-    <div class="dialog"> <img src="images/icone1.png" alt="Técnica Mobile First">
+    <div class="dialog"> <img src="images_m/icone1.png" alt="Técnica Mobile First">
       <h3 title="Técnica Mobile First">Mobile First</h3>
       <p>Os dispositivos móveis estão tomando conta do mundo. Sendo assim, o que você acha de pensarmos nos dispositivos móveis antes de pensarmos nos desktops? (fonte Tableless.com.br)</p>
     </div>
     <!--end .dialog-->
 
-    <div class="dialog"> <img src="images/icone1.png" alt="Referência Tableless">
+    <div class="dialog"> <img src="images_m/icone1.png" alt="Referência Tableless">
       <h3 title="Técnicas Tableless">Tableless</h3>
       <p>Experiência em desenvolvimento tableless, aplicando as melhores práticas existentes no mercado em projetos.</p>
     </div>
     <!--end .dialog-->
 
-    <div class="dialog_last"> <img src="images/icone1.png" alt="Trabalhos Freelancer">
+    <div class="dialog_last"> <img src="images_m/icone1.png" alt="Trabalhos Freelancer">
       <h3 title="Trabalhos Freelancer">Freelancer</h3>
       <p>Vasta Experiência em trabalhos Freelancer, em parceria com agências de grande porte no mercado e , clientes informais</p>
     </div>
@@ -93,129 +92,94 @@
   <h2 title="Últimos Jobs">Trabalhos Recentes</h2>
   <ul class="grid cs-style-6">
     <li>
-      <figure> <img src="images/vista_verde.png" alt="Clube Golfe Vista Verde">
-        <figcaption>
-          <h3>Vista Verde</h3>
-          <span>Clube Golfe Vista Verde</span> <a href="http://www.wagneramos.com.br/clients/vista" target="_blank">Visitar Projeto</a> </figcaption>
-      </figure>
-    </li>
-    <li>
-      <figure> <img src="images/lobrobo.png" alt="Lobrobo">
-        <figcaption>
-          <h3>Lobrobo</h3>
-          <span>Restaurante Secreto</span> <a href="http://lobrobo.com.br" target="_blank">Visitar Projeto</a> </figcaption>
-      </figure>
-    </li>
-    <li>
-      <figure> <a class="fancybox" href="images/lightbox/tim/dnaum.jpg" data-fancybox-group="gallery"><img src="images/dna.png" alt="DNA"></a> <a class="fancybox" href="images/lightbox/tim/dnadois.jpg" data-fancybox-group="gallery"></a> <a class="fancybox" href="images/lightbox/tim/dnatres.jpg" data-fancybox-group="gallery"></a> <a class="fancybox" href="images/lightbox/tim/dnaquatro.jpg" data-fancybox-group="gallery"></a>
-        <figcaption>
-          <h3>DNA TIM</h3>
-          <span>incentivo de vendas</span> <a  href="images/lightbox/tim/dnadois.jpg" class="fancybox" data-fancybox-group="gallery"> Fotos Projeto</a> </figcaption>
-      </figure>
-    </li>
-    <li>
-      <figure> <img src="images/cineasta.jpg" alt="Cineasta">
+      <figure> <img src="images_m/cineasta.jpg" alt="Cineasta">
         <figcaption>
           <h3>Cineasta</h3>
           <span>Cineasta Crowdfunding</span> <a href="http://www.cineasta.cc" target="_blank">Visitar Projeto</a> </figcaption>
       </figure>
     </li>
     <li>
-      <figure> <img src="images/linka.jpg" alt="linka">
+      <figure> <img src="images_m/linka.jpg" alt="linka">
         <figcaption>
           <h3>Linka</h3>
           <span>Linka</span> <a href="http://linka.vc" target="_blank">Visitar Projeto</a> </figcaption>
       </figure>
     </li>
     <li>
-      <figure> <img src="images/tilkian.jpg" alt="Decoussau Tilkian Advogados">
+      <figure> <img src="images_m/tilkian.jpg" alt="Decoussau Tilkian Advogados">
         <figcaption>
           <h3>Dtlaw</h3>
           <span>Decoussau Tilkian Advogados</span> <a href="http://www.dtlaw.com.br/en" target="_blank">Visitar Projeto</a> </figcaption>
       </figure>
     </li>
     <li>
-      <figure> <img src="images/gentileza.jpg" alt="Gentileza gera Sobremesa">
+      <figure> <img src="images_m/gentileza.jpg" alt="Gentileza gera Sobremesa">
         <figcaption>
           <h3>Gentileza gera Sobremesa</h3>
           <span>Promoção The Fifties</span> <a href="http://www.wagneramos.com.br/clients//Promo_fifities/" target="_blank">Visitar Projeto</a> </figcaption>
       </figure>
     </li>
     <li>
-      <figure> <img src="images/ebs.png" alt="EBS">
-        <figcaption>
-          <h3>EBS</h3>
-          <span>Enterprise Business Solutions </span> <a href="http://www.ebsolutions.com.br/" target="_blank">Visitar Projeto</a> </figcaption>
-      </figure>
-    </li>
-    <li>
-      <figure> <img src="images/rolinardi.png" alt="Rolinardi">
-        <figcaption>
-          <h3>Rolinardi</h3>
-          <span>Fotografia Intimista</span> <a href="http://rolinardi.com.br" target="_blank">Visitar Projeto</a> </figcaption>
-      </figure>
-    </li>
-    <li>
-      <figure> <img src="images/jobs_umbro.jpg" alt="Umbro">
+      <figure> <img src="images_m/jobs_umbro.jpg" alt="Umbro">
         <figcaption>
           <h3>Umbro</h3>
           <span>Fichas e-commerce Umbro</span> <a href="http://www.wagneramos.com.br/clients/umbro_vasco/umbro_vasco/" target="_blank">Visitar Projeto</a> </figcaption>
       </figure>
     </li>
     <li>
-      <figure> <img src="images/most_asus.png" alt="Asus">
+      <figure> <img src="images_m/most_asus.png" alt="Asus">
         <figcaption>
           <h3>Asus</h3>
           <span>Hot site Asus para Loja Fnac</span> <a href="http://www.wagneramos.com.br/clients/Asus/BlackFriday_Asus" target="_blank">Visitar Projeto</a> </figcaption>
       </figure>
     </li>
     <li>
-      <figure> <img src="images/top_brands_port.jpg" alt="Top Brands">
+      <figure> <img src="images_m/top_brands_port.jpg" alt="Top Brands">
         <figcaption>
           <h3>Top Brands</h3>
           <span>Site Top Brands</span> <a href="http://www.topbrands.com.br" target="_blank">Visitar Projeto</a> </figcaption>
       </figure>
     </li>
     <li>
-      <figure> <img src="images/conte.jpg" alt="Conte">
+      <figure> <img src="images_m/conte.jpg" alt="Conte">
         <figcaption>
           <h3>Conte</h3>
           <span>Site da Empresa Conte</span> <a href="http://www.http://www.conte.com.br/" target="_blank">Visitar Projeto</a> </figcaption>
       </figure>
     </li>
     <li>
-      <figure> <img src="images/zenfone_zoom.png" alt="Azus Zenfone Zoom">
+      <figure> <img src="images_m/heineken.png" alt="Heineken">
         <figcaption>
-          <h3>Zenfone Zoom</h3>
-          <span>Azus Zenfone Zoom</span> <a href="http://www.wagneramos.com.br/clients/zoom" target="_blank">Visitar Projeto</a> </figcaption>
+          <h3>Hot Site Heineken</h3>
+          <span>Promoção Heineken</span> <a href="http://www.heineken.com.br/delegates/home/" target="_blank">Visitar Projeto</a> </figcaption>
       </figure>
     </li>
     <li>
-      <figure> <img src="images/dani-derani.jpg" alt="Dani Derani">
+      <figure> <img src="images_m/dani-derani.jpg" alt="Dani Derani">
         <figcaption>
           <h3>Dani Ferani</h3>
           <span>Fotógrafa Dani Ferani</span> <a href="http://www.daniderani.com.br" target="_blank">Visitar Projeto</a> </figcaption>
       </figure>
     </li>
     <li>
-      <figure> <img src="images/asus_live.png" alt="Asus Live">
+      <figure> <img src="images_m/asus_live.png" alt="Asus Live">
         <figcaption>
           <h3>Asus Live</h3>
           <span>Asus Live</span> <a href="http://www.wagneramos.com.br/clients/Asus/asus_live" target="_blank">Visitar Projeto</a> </figcaption>
       </figure>
     </li>
     <li>
-      <figure> <img src="images/diamond.jpg" alt="Umbro Diamond">
+      <figure> <img src="images_m/diamond.jpg" alt="Umbro Diamond">
         <figcaption>
           <h3>Umbro Diamond</h3>
           <span>Chuteiras Umbro</span> <a href="http://www.wagneramos.com.br/clients/Diamond/Diamond/" target="_blank">Visitar Projeto</a> </figcaption>
       </figure>
     </li>
     <li>
-      <figure> <a class="fancybox" href="images/lightbox/bmw/BMWMotorrad01_01.jpg" data-fancybox-group="gallery"><img src="images/BMWMotorradSelect.jpg" alt="BMW Motorrad"></a> <a class="fancybox" href="images/lightbox/bmw/BMWMotorrad02_01.jpg" data-fancybox-group="gallery"></a>
+      <figure> <img src="images_m/1.png" alt="circuito da autencidade">
         <figcaption>
-          <h3>BMW Motorrad</h3>
-          <span>BMW Motorrad</span><a href="images/lightbox/bmw/BMWMotorrad01_01.jpg" class="fancybox" data-fancybox-group="gallery"> Fotos Projeto</a> </figcaption>
+          <h3>Circuito da Autencidade</h3>
+          <span>Promoção Circuito da Autencidade</span> <a href="http://circuitodaautenticidade.com.br/" target="_blank">Visitar Projeto</a> </figcaption>
       </figure>
     </li>
   </ul>
@@ -225,26 +189,26 @@
 <div id="skills" >
   <div class="inner_skills">
     <h2 title="Habilidades e Know how">Habilidades</h2>
-    <div class="skills wow bounceInUp"> <img src="images/html5.png" alt="html 4  e 5" />
+    <div class="skills wow bounceInUp"> <img src="images_m/html5.png" alt="html 4  e 5" />
       <h1>HTML 5</h1>
     </div>
-    <div class="skills wow bounceInUp" data-wow-delay="1s"> <img src="images/css.png" alt="css2 e css3" />
+    <div class="skills wow bounceInUp" data-wow-delay="1s"> <img src="images_m/css.png" alt="css2 e css3" />
       <h2 class="css">css</h2>
     </div>
-    <div class="skills wow bounceInUp" data-wow-delay="2s"> <img src="images/csst.png" alt="javascript" />
+    <div class="skills wow bounceInUp" data-wow-delay="2s"> <img src="images_m/csst.png" alt="javascript" />
       <h3>js</h3>
     </div>
-    <div class="skills wow bounceInUp " data-wow-delay="3s"> <img src="images/grunt.png" alt="task runner grunt" />
+    <div class="skills wow bounceInUp " data-wow-delay="3s"> <img src="images_m/grunt.png" alt="task runner grunt" />
       <h4 class="grunt">grunt</h4>
     </div>
-    <div class="skills wow bounceInUp" data-wow-delay="4s"> <img src="images/jquery.png" alt="plugins e programação jquery" />
+    <div class="skills wow bounceInUp" data-wow-delay="4s"> <img src="images_m/jquery.png" alt="plugins e programação jquery" />
       <h5 class="jquery">jquery</h5>
     </div>
-    <div class="skills wow bounceInUp"  data-wow-delay="5s"> <img src="images/photoshop.png" alt="programa photoshop" />
+    <div class="skills wow bounceInUp"  data-wow-delay="5s"> <img src="images_m/photoshop.png" alt="programa photoshop" />
       <h6 class="photoshop">photoshop</h6>
     </div>
-    <div class="skills wow bounceInUp"  data-wow-delay="6s"> <img src="images/sass.png" alt="Sass Framework" />
-      <h6 class="photoshop">SASS</h6>
+    <div class="skills wow bounceInUp"  data-wow-delay="6s"> <img src="images_m/ico_axure.png" alt="programa Axure" />
+      <h6 class="photoshop">Axure</h6>
     </div>
   </div>
   <!--end inner_skills-->
@@ -254,7 +218,7 @@
 
 <div id="footer">
   <div class="inner_footer">
-    <div class="logo_footer"> <img src="images/logo_wagner_ramos.png" alt="Wagner Ramos"/> </div>
+    <div class="logo_footer"> <img src="images_m/logo_wagner_ramos.png" alt="Wagner Ramos"/> </div>
     <div class="menu_footer">
       <ul>
         <li role="menuitem"><a href="#slider">Home</a></li>
@@ -268,9 +232,9 @@
       <p>telefone:<strong> +55 11 976785776</strong></p>
       <p>Email:<strong> qa302655@gmail.com</strong></p>
       <ul>
-        <li><a href="https://br.linkedin.com/in/wagner-ramos-53a3116" target="_blank"><img src="images/linkedin_circle_color-512.png" alt="Linkedin" title="Acessar Perfil Linkedin" /></a></li>
-        <li><a href="images/Jose_Wagner_Silva_Ramos_atual.pdf" target="_blank"><img src="images/pdf.png" alt="Download Curriclum" title="Download Curriculum" /></a></li>
-        <li><a href="callto:jose.wagner.silva.ramos"><img src="images/icon_skype.png" alt="Skype" title="Skype" /></a></li>
+        <li><a href="https://br.linkedin.com/in/wagner-ramos-53a3116" target="_blank"><img src="images_m/linkedin_circle_color-512.png" alt="Linkedin" title="Acessar Perfil Linkedin" /></a></li>
+        <li><a href="images_m/Jose_Wagner_Silva_Ramos_atual.pdf" target="_blank"><img src="images_m/pdf.png" alt="Download Curriclum" title="Download Curriculum" /></a></li>
+        <li><a href="callto:jose.wagner.silva.ramos"><img src="images_m/icon_skype.png" alt="Skype" title="Skype" /></a></li>
       </ul>
     </div>
     <p class="copy">© Wagner Ramos 2016. Todos os direitos reservados.</p>
@@ -290,10 +254,15 @@
   ga('send', 'pageview');
 
 </script>
-<script   src="https://code.jquery.com/jquery-1.12.3.min.js"   integrity="sha256-aaODHAgvwQW1bFOGXMeX+pC4PZIPsvn2h1sArYOhgXQ="   crossorigin="anonymous"></script>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/jquery.slicknav.min.js"></script>
 <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
+<script type="text/javascript" src="js/jquery.flexslider.js"></script>
+<script type="text/javascript" src="js/stellar.js"></script>
 <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
-<script type="text/javascript" src="js/built.js"></script>
-<script type="text/javascript" src="http://www.google-analytics.com/analytics.js"></script>
+<script src="js/anima_header.js"></script>
+<script type="text/javascript" src="js/toucheffect.js"></script>
+<script type="text/javascript" src="js/wow.min.js"></script>
+<script type="text/javascript" src="js/all.js"></script>
 </body>
 </html>
---
+++ BR/Gruntfile.js
@@ -0,0 +1,87 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+pkg: grunt.file.readJSON('package.json'),
+
+//css min
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'css',
+          src: ['*.css', '!*.min.css'],
+          dest: 'css',
+          ext: '.min.css'
+        }]
+      }
+    },
+
+//concat
+    concat_css: {
+        options: {
+          // Task-specific options go here.
+        },
+        all: {
+          src: ["/**/*.css"],
+          dest: "main.min.css"
+        },
+      },
+
+//imagemin
+      imagemin: {
+          dynamic: {
+              files: [{
+                  expand: true,
+                  cwd: 'images_m/',
+                  src: ['**/*.{png,jpg,gif}'],
+                  dest: 'images/'
+              }]
+          }
+      },
+
+//concat JS
+    concat: {
+        options: {
+          separator: ';',
+        },
+        dist: {
+          src: [ 'js/jquery.js', 'js/anima_header.js', 'js/jquery.flexslider.js', 'js/jquery.slicknav.min.js', 'js/stellar.js','js/toucheffect.js', 'js/wow.min.js','js/jquery.fancybox.pack.js', 'js/all.js'],
+          dest: 'js/built.js',
+        },
+      },
+
+
+// Project configuration.
+
+  uglify: {
+    my_target: {
+      files: {
+        'js/built.js': ['js/jquery.js', 'js/anima_header.js', 'js/jquery.flexslider.js', 'js/jquery.slicknav.min.js', 'js/stellar.js','js/toucheffect.js', 'js/wow.min.js','js/jquery.fancybox.pack.js', 'js/all.js']
+      }
+    }
+  }
+
+
+
+
+
+
+
+});
+
+//Load the plugin
+
+grunt.loadNpmTasks('grunt-contrib-cssmin');
+grunt.loadNpmTasks('grunt-contrib-imagemin');
+grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-concat-css');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+
+//grunt.registerTask('default', ['concat', 'uglify', 'imagemin']);
+
+grunt.registerTask('default', ['cssmin']);
+grunt.registerTask('default', ['imagemin']);
+grunt.registerTask('default', ['concat']);
+
+};
